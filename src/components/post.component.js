a16f132599ab0a8aa6ea906f4c35ea3adbb86381
Max44444 <Skorokhodov.m.v@ukr.net>
2021-01-16 19:26:47 +0200

    Show posts list

--- src/components/post.component.js
+++ src/components/post.component.js
@@ -10,6 +10,37 @@ export class PostComponent extends Component {
   async onShow() {
     const fbData = await apiService.fetchPosts();
     const posts = transformService.fbObjectToArray(fbData);
-    console.log(posts);
+    const html = posts.map(renderPost).join(' ');
+
+    this.$el.insertAdjacentHTML('afterbegin', html)
   }
+
+  onHide() {
+    this.$el.innerHTML = '';
+  }
+}
+
+function renderPost({title, type, fulltext, date}) {
+  const tag = type === 'news'
+    ? '<li class="tag tag-blue tag-rounded">Новость</li>'
+    : '<li class="tag tag-rounded">Заметка</li>';
+
+  const button = '<button class="button-round button-small button-primary">Сохранить</button>';
+
+  return `
+    <div class="panel">
+      <div class="panel-head">
+        <p class="panel-title">${title}</p>
+        <ul class="tags">
+          ${tag}
+        </ul>
+      </div>
+      <div class="panel-body">
+        <p class="multi-line">${fulltext}</p>
+      </div>
+      <div class="panel-footer w-panel-footer">
+        <small>${date}</small>
+        ${button}
+      </div>
+    </div>`
 }
